f7ff0e02db756340aa9ce21977f0eb8422267f68
Andrii_Olefirenko <andreyolef@gmail.com>
2024-09-07 10:28:16 +0300

    telemart href added

--- tests/telemart-carousel.spec.ts
+++ tests/telemart-carousel.spec.ts
@@ -3,10 +3,12 @@ import { test, expect } from "@playwright/test";
 test("Jira-01 Telemart carousel is scrolled and redirects to correct page", async ({
   page,
 }) => {
-  const banner3URL =
-    "https://telemart.ua/ua/promotions/gift-assembly-service.html";
-
   await page.goto("https://telemart.ua/ua/");
+
+  const banner3URL = await page
+    .locator('//a[@data-banner-position="3"]')
+    .getAttribute("href");
+
   expect(
     (await page.locator(".swiper-on-main .swiper-wrapper a").all()).length
   ).toBeGreaterThan(2);
